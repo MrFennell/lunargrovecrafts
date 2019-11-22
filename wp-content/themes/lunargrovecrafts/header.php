7cea6a9a999795eda6d44ec606cf87e185801196
Ryan <rfennell001@gmail.com>
2019-11-22 12:27:20 -0500

    Added cart counter to icon

--- wp-content/themes/lunargrovecrafts/header.php
+++ wp-content/themes/lunargrovecrafts/header.php
@@ -25,8 +25,6 @@
 	<a class="sr-only sr-only-focusable" href="#content"><?php esc_html_e( 'Skip to content', 'lunargrovecrafts' ); ?></a>
 
 	<header id="masthead" class="site-header">
-		
-		
 			<div id="hero"><!-- header for front page-->
                 <?php if( has_post_thumbnail() && is_front_page() ):
                     $post = get_post();
@@ -38,7 +36,7 @@
                         }
                     </style>
                 <?php endif; ?>
-				<nav id="site-navigation" class="navbar navbar-dark navbar-expand-md bg-dark"><!-- #site-navigation -->
+				<nav id="site-navigation" class="navbar navbar-dark navbar-expand-lg bg-dark"><!-- #site-navigation -->
                     <?php $logo = get_custom_logo();
                     if ($logo):
                         echo get_custom_logo();
@@ -62,7 +60,19 @@
                     <div id="navbarNav" class="collapse navbar-collapse">
                         <ul id="account-cart" class="navbar-nav ml-auto">
                             <li><a title="My Account" href="<?php echo get_home_url(); ?>/my-account" class="nav-link">My Account</a></li>
-                            <li><a title="Cart" href="<?php echo get_home_url(); ?>/cart" class="nav-link" aria-current="page"><span class="icon-cart"><span class="text-hide">Cart</span></span></a></li>
+                            <li>
+                                <div id="cart-container">
+                                <?php echo do_shortcode("[woo_cart_but]"); ?>
+                                    <a title="Cart" href="<?php echo get_home_url(); ?>/cart" class="nav-link" aria-current="page">
+                                    
+
+                                        <span class="icon-cart">
+                                        
+                                            <span class="text-hide">Cart</span>
+                                        </span>
+                                    </a>
+                                </div>
+                            </li>
                         </ul>
                     </div>
                 </nav>
